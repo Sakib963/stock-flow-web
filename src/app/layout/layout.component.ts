1b0477a59e25498ea271a3724a2c2e3b1c8fc0fe
Sakib963 <abdullah15-963@diu.edu.bd>
2024-11-20 23:21:41 +0600

    added logout feat

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -9,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  constructor(private _authService: AuthService) {}
+  handleLogout(): any {
+    this._authService.logout();
+  }
+}
