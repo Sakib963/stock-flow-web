c544cbbd5e911506cfbb77ac4cf37d6fcc49401e
Sakib963 <abdullah15-963@diu.edu.bd>
2025-01-07 23:08:30 +0600

    fixed sidebar

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -4,6 +4,7 @@ import { LayoutComponent } from './layout.component';
 import { UserInfoResolver } from '@app/core/resolvers/user-info.resolver';
 import { RoleGuard } from '@app/core/guards/role.guard';
 import { ROLES } from '@app/core/constants/constants';
+import { NotFoundComponent } from '@app/shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -24,6 +25,14 @@ const routes: Routes = [
           import('../modules/manager/manager.module').then((m) => m.ManagerModule),
         canActivate: [RoleGuard],
         data: { roles: [ROLES.MANAGER] },
+      },
+      {
+        path: 'not-found',
+        component: NotFoundComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'not-found',
       },
     ],
   },
