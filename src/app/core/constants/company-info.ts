1d58fd6dd6856b166a0ff45340e8569751db1be8
sakib963 <abdullah15-963@diu.edu.bd>
2025-10-28 08:59:44 +0600

    feat (report): Inventory

--- src/app/core/constants/company-info.ts
+++ src/app/core/constants/company-info.ts
@@ -1,8 +1,8 @@
 // here we need to define the company information
 export const COMPANY_INFO = {
-  name: 'Bondhu Sports Plus',
-  address: '123 Innovation Drive, Tech City, TC 12345',
+  name: 'StockFlow Inc.',
+  address: '123 Stock Management St., Stock City',
   phone: '+1 (555) 123-4567',
-  email: 'info@bondhusports.com',
-  website: 'www.bondhusports.com'
+  email: 'info@stockflow.com',
+  website: 'www.stockflow.com'
 };
